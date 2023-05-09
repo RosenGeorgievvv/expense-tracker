2b424adad957b00d642a6a79018373bd5eee8421
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-05-09 13:13:10 +0300

    expense-tracker project finished
    A simple expense tracker, where you can add a new transaction or delete a transaction. Build using React JS -> Hooks -> React Context

--- src/context/GlobalState.jsx
+++ src/context/GlobalState.jsx
@@ -4,13 +4,7 @@ import AppReducer from './AppReducer'
 
 //Initial state
 const initialState = {
-    transactions: [
-          { id: 1, text: 'Flower', amount: -20 },
-          { id: 2, text: 'Salary', amount: 300 },
-          { id: 3, text: 'Book', amount: -10 },
-          { id: 4, text: 'Camera', amount: 150 }
-         ]
-        
+    transactions: []     
 }
 
 export const GlobalContext = createContext(initialState);
@@ -24,10 +18,17 @@ function deleteTransaction(id){
         payload: id
     })
 }
+function addTransaction(transaction){
+    dispatch({
+        type: 'ADD_TRANSACTION',
+        payload: transaction
+    });
+}
 
 return (<GlobalContext.Provider value={{
     transactions: state.transactions,
-    deleteTransaction
+    deleteTransaction,
+    addTransaction
 }}>
     {children}
 </GlobalContext.Provider>)
