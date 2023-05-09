8851a5fef23dcc4e86c2638682d4b26c23b068d5
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-05-09 10:07:56 +0300

    Update styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Getting Started</h1>
+    <div>
+      <Header />
     </div>
   );
 }
